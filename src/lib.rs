94bb2760feb0487429691cbf8771b764b4b8d95d
axumrs <rolson867@gmail.com>
2024-07-03 10:14:15 +0800

    `hi()`方法返回字段列表

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,26 @@ pub fn db_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
     let name = ast.ident;
 
+    let fileds = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }, ..),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unreachable!()
+    };
+
+    let field_str = fileds
+        .iter()
+        .map(|f| f.ident.clone().unwrap().to_string())
+        .collect::<Vec<_>>()
+        .join(",");
+
     quote::quote! {
         impl #name {
             pub fn hi(&self) -> &'static str {
-                "Hello, axum.rs!"
+                #field_str
             }
         }
     }
